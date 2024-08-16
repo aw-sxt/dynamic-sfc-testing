1fc5542ec5254c99ba199acaf766947b53ad135f
aw-sxt <alex.williams@spaceandtime.io>
2024-08-16 10:33:42 -0500

    init

---
+++ components/counter.vue
@@ -0,0 +1,16 @@
+<template>
+	<div>
+		<span>{{ count }}</span>
+		<button v-on:click="incrementCount">Increment</button>
+	</div>
+</template>
+
+<script setup>
+import {ref} from 'vue';
+const count =? ref(0);
+
+function incrementCount() {
+	count.value++;
+}
+
+</script>
---
+++ components/foo.vue
@@ -0,0 +1,12 @@
+<template>
+	<div>
+		<p>Message is: {{ message }}</p>
+		<input v-model="message" placeholder="edit me" />
+	</div>
+</template>
+
+<script setup>
+import {ref} from 'vue';
+const message = ref(null);
+
+</script>
